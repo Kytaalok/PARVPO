e6301b37b4141d092d51955f89c510f6c70a5834
Pavel Artemov <89926298+Kytaalok@users.noreply.github.com>
2024-12-24 17:44:12 +0300

    Test

--- Lab1/cpu_load.cpp
+++ Lab1/cpu_load.cpp
@@ -1,77 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
 #include <time.h>
-#include <stdint.h>
 
-uint32_t xorshift32(uint32_t *seed) {
-    uint32_t x = *seed;
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return *seed = x;
-}
-
-void shell_sort(uint32_t *array, int left, int right) {
-    int gap, i, j;
-    uint32_t temp;
-    for (gap = (right - left + 1) / 2; gap > 0; gap /= 2) {
-        for (i = left + gap; i <= right; i++) {
-            temp = array[i];
-            for (j = i; j >= left + gap && array[j - gap] > temp; j -= gap) {
-                array[j] = array[j - gap];
-            }
-            array[j] = temp;
-        }
-    }
+void benchmark() {
+    long long count = 0;
+    for (long long i = 0; i < 20000000000; i++) { count++; }
 }
 
 int main() {
-    clock_t all_time_start = clock();
+    clock_t start, end;
+    double cpu_time_used;
 
-    const int count = 10000000;
-    uint32_t random_seed = 90214;
-    double time_spent = 0;
-    int part_size = 0;
+    start = clock();
+    benchmark();
+    end = clock();
 
-    uint32_t *array = (uint32_t *) malloc(count * sizeof(uint32_t));
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-    //printf("OpenMP: %d;\n======\n", _OPENMP);
-    const int runs_num = 20;
+    printf("The task took %f seconds to execute.\n", cpu_time_used);
 
-    int t = 12;
-    for (int i = 0; i < runs_num; i++) {
-
-        for (int i = 0; i < count; i++) {
-            array[i] = xorshift32(&random_seed);
-        }
-
-        part_size = count / t;
-
-        clock_t begin = clock();
-
-        #pragma omp parallel num_threads(t) shared(array, count, part_size, t) default(none)
-        {
-            int thread_id = omp_get_thread_num();
-            int left = thread_id * part_size;
-            int right = (thread_id == t - 1) ? count - 1 : (left + part_size - 1);
-
-            shell_sort(array, left, right);
-        }
-        
-        shell_sort(array, 0, count);
-
-        clock_t end = clock();
-        time_spent += (double) (end - begin) / CLOCKS_PER_SEC;
-        printf("Seconds spent: %lf\n", time_spent);
-    }
-    // printf("Threads: %d\nSeconds spent: %lf\n", t, time_spent / runs_num);
-    time_spent = 0;
-    random_seed = 90214;
-
-    clock_t all_time_end = clock();
-    double all_time = (double) (all_time_end - all_time_start) / CLOCKS_PER_SEC;
-
-    printf("Seconds spent: %lf\n", all_time);
     return 0;
 }
